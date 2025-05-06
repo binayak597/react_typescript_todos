4569aadf4bffc05b0f5633a025b2198c478a5fdc
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-05-06 23:24:47 +0530

    initial layout ready

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import InputField from './components/InputField'
+
+import './App.css'
+
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+
+      <span className='heading'>Todos</span>
+      <InputField />
+    </div>
+  )
+}
+
+export default App
